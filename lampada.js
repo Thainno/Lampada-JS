ffff440802a3cd97eafbab957b4588cb2b381212
Thainno Santana <thainnosv@gmail.com>
2022-10-16 13:23:58 -0300

    Finalização

---
+++ lampada.js
@@ -0,0 +1,37 @@
+const turnOn = document.getElementById ("turnOn")
+const turnOff = document.getElementById ("turnOff")
+const lamp = document.getElementById ("lamp")
+
+
+function isLampBroken () {
+    return lamp.src.indexOf("quebrada") > -1
+}
+
+function lampOn () {
+    if (!isLampBroken()) {
+        lamp.src = "/imagens/ligada.jpg"; 
+        lamp.style.background = "radial-gradient(rgb(187, 187, 39),rgb(122, 122, 40)30%, #1e1e1e 70%)"; 
+    }
+}
+
+function lampOff () {
+    if (!isLampBroken()) {
+        lamp.src = "/imagens/desligada.jpg";
+        lamp.style.background = "none";
+    }
+}
+
+function lampBroken () {
+    if (!isLampBroken()) {
+        lamp.src = "/imagens/quebrada.jpg";
+    }
+}
+
+turnOn.addEventListener("click", lampOn)
+turnOff.addEventListener("click", lampOff)
+lamp.addEventListener("mouseover", lampOn)
+lamp.addEventListener("mouseleave", lampOff)
+lamp.addEventListener("dblclick", lampBroken)
+
+
+
